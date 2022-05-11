d358a6cffdcbbff80d021456766953832ff7ad5a
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2022-05-11 17:09:29 +0300

    gather telemetry reports

--- server/reportTelemetry.go
+++ server/reportTelemetry.go
@@ -1,16 +1,26 @@
 package server
 
 import (
+	"io"
+
 	"gitflic.ru/vodolaz095/control/pb"
-	"log"
 )
 
-func (rpc *RPC) ReportTelemetry(server pb.Control_ReportTelemetryServer) (err error) {
-	telemetry, err := server.Recv()
-	if err != nil {
-		log.Printf("%s : while receiving telemetry", err)
-		return
+// Good read: https://grpc.io/docs/languages/go/basics/#server-side-streaming-rpc
+
+// ReportTelemetry gathers telemetry reports
+func (rpc *RPC) ReportTelemetry(server pb.Control_ReportTelemetryServer) error {
+	for {
+		telemetry, err := server.Recv()
+		if err == io.EOF {
+			err = server.SendAndClose(&pb.StringResponse{Data: "Farewell"})
+			if err != nil {
+				return err
+			}
+		}
+		if err != nil {
+			return err
+		}
+		rpc.Logger.Printf("Telemetry received %v %v %v", telemetry.Load1, telemetry.Load2, telemetry.Load3)
 	}
-	log.Printf("Telemetry received %v %v %v", telemetry.Load1, telemetry.Load2, telemetry.Load3)
-	return
 }
